2d12171eff2098e88f03561e2ed88f7ad9223b1d
Jordi Prats <jordi.prats@gmail.com>
2026-03-12 16:00:45 +0100

    show install status

--- localinstall.sh
+++ localinstall.sh
@@ -8,6 +8,8 @@ go build -o dist/iam-pb-check main.go
 # Run tests
 bash test.sh
 
+echo "Binary built and tests passed. Installing to $HOME/local/bin..."
+
 # Install
 mkdir -p $HOME/local/bin
 mv dist/iam-pb-check $HOME/local/bin/iam-pb-check
