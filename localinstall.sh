5eee422310284d2b5bf823a6793329df12d6e242
Jordi Prats <jordi.prats@gmail.com>
2026-03-19 11:47:23 +0100

    cleanup

--- localinstall.sh
+++ localinstall.sh
@@ -13,6 +13,5 @@ echo "Binary built and tests passed. Installing to $HOME/local/bin..."
 # Install
 mkdir -p $HOME/local/bin
 mv dist/iamctl $HOME/local/bin/iamctl
-rm -f $HOME/local/bin/iam-pb-check $HOME/local/bin/ec2-pb-check
-ln -sf $HOME/local/bin/iamctl $HOME/local/bin/iam-pb-check
-ln -sf $HOME/local/bin/iamctl $HOME/local/bin/ec2-pb-check
+
+echo "Installation complete."
